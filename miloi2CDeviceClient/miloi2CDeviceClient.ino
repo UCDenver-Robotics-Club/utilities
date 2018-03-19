833b264c2ee7b319bf03617ba88c4c0d27e97694
dillon <dhall@motocol.com>
2018-03-19 16:54:21 -0600

    added json command functionality to the led blinker function

--- miloi2CDeviceClient/miloi2CDeviceClient.ino
+++ miloi2CDeviceClient/miloi2CDeviceClient.ino
@@ -1,12 +1,19 @@
 #include <Wire.h>
 #include <ArduinoJson.h>
 #include <stdint.h>
+#include <string.h> // c string lib, sometimes faster
+#include <avr/io.h>
+
+bool isRunning;
 
 void setup() 
 {
   Wire.begin(8); // join as address 8
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
+  isRunning = false;
+  // set pin 13 to output
+  DDRB |= (1<<PB5);
 }
 
 void loop() 
@@ -26,5 +33,36 @@ void receiveEvent(int howMany)
   }
 
   commandBuffer[index] = '\0';
-  Serial.println(commandBuffer);
+  //Serial.println(commandBuffer);
+  parseCommand(String(commandBuffer));
 }
+
+void parseCommand(String rawcommand)
+{
+  // convert the string from raw text to an object
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(rawcommand);
+  const char* device = root["device"];
+  const char* action = root["action"];
+
+  if(strcmp(device,"led") == 0)
+  {
+    setLed(action);
+  }
+  
+}
+
+
+void setLed(char *action)
+{
+  Serial.println(action);
+  if(strcmp(action,"on") == 0)
+  {
+    PORTB |= (1<<PB5);
+  }
+  else if(strcmp(action,"off") == 0)
+  {
+    PORTB &= ~(1 << PB5);
+  }
+}
+
